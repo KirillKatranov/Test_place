2df6922cd464d65d1711e0fb33aee13522440211
Кирилл Катранов <katranovrpk@gmail.com>
2025-06-12 10:27:48 +0300

    Free Write Decorators 2

--- SomeFreeWrite.py
+++ SomeFreeWrite.py
@@ -6,18 +6,23 @@ def measure_time(parametr):
     def real_decorator(func):
         def wrapper(*args, **kwargs):
             start = time.time()
-            func()
+            nonlocal parametr 
+            parametr += "Wow"
+            print(parametr + "Wow")
+            func(parametr)
             end = time.time()
             return end - start
         return wrapper
     return real_decorator
 
 
-@measure_time(parametr1 = "some_string")    #some_func = measure_time(parametr="some_string")(some_func)
+@measure_time(parametr = "some_string")    #some_func = measure_time(parametr="some_string")(some_func)
 def some_func(*args, **kwargs):
     time.sleep(5)
     print("Я функция")
+    print(args)
+    print(*args[0])
+    print(args[0])
 
 if __name__ == "__main__":
-
     print(some_func(), "сек")
